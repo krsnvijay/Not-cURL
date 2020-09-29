bb1857ff77e63ce9d85df501af9181b61760ad48
Not A Hero <krsnvijay@gmail.com>
2020-09-29 19:39:55 -0400

    Support Multiple Headers

--- notcurl.py
+++ notcurl.py
@@ -6,33 +6,37 @@ from urllib.parse import urlparse
 
 """
 Things to do:
-1. Change the help message to display help for each command
-2. Fix UGLY code
+1. Change the help message to display help for each command 
+2. Fix UGLY code :)
 3. Headers aren't properly formatted
-4. Saving to file always writes headers too. Can use if/else but it's very redundant so idk
+4. Saving to file always writes headers too. Can use if/else but it's very redundant so idk :|
 5. Redirection (requests.get allows redirection by default)
-6. changes to args.h so it works for multiple headers
-7. Haven't tested -f
+6. changes to args.h so it works for multiple headers  :)
+7. Haven't tested -f :|
 """
-
-# -v, -h(works with "key:value"): Optional Argument, URL(has to be split) : Positional argument
-# get|post: group arguments, Post can either have -d,-f but not both (optional arguments). GET has nothing
+def headerToDict (headers):
+    result = {}
+    for header in headers:
+        key,value = header.split(':')
+        result[key] = value
+    return result
+# -v, -h(works with "key:value"): Optional Argument, URL(has to be split) : Positional argument 
+#get|post: group arguments, Post can either have -d,-f but not both (optional arguments). GET has nothing
 parser = argparse.ArgumentParser(
-    description="httpc is a curl-like application but supports HTTP protocol only.",
-    formatter_class=argparse.RawDescriptionHelpFormatter,
+    description="httpc is a curl-like application but supports HTTP protocol only.", 
+    formatter_class= argparse.RawDescriptionHelpFormatter,
+    usage='''\n%(prog)s command [arguments]''',
     prog="httpc",
     add_help=False,
     epilog='Use "httpc help [command]" for more information about a command.')
 
-parser.add_argument("-help", action='help', help='prints this screen')
-parser.add_argument("-v", "--verbosity",
-                    help="Prints the detail of the response such as protocol, status, and headers.", action="store_true")
-parser.add_argument("-h", help="Associates headers to HTTP Request with the format 'key:value'.",
-                    metavar="k:v", action='append', nargs="*")
+group = parser.add_mutually_exclusive_group(required=True)
+group.add_argument("-get", action="store_true", help="executes a HTTP GET request and prints the response.", dest='get')
+group.add_argument("-post", action="store_true",help="executes a HTTP POST request and prints the response.", dest='post')
 
 parser.add_argument("-v","--verbosity", help="Prints the detail of the response such as protocol, status, and headers.", action="store_true")
 parser.add_argument("-help",action='help', help='prints this screen')
-parser.add_argument("-h", help="Associates headers to HTTP Request with the format 'key:value'.", metavar="k:v", action='append', nargs="*")
+parser.add_argument("-h", help="Associates headers to HTTP Request with the format 'key:value'.", metavar="k:v", action='append')
 
 parser.add_argument("-d",help="Associates an inline data to the body HTTP POST request.", metavar="inline-data")
 parser.add_argument("-f",help="Associates the content of a file to the body HTTP POST request.", metavar="file")
@@ -40,6 +44,7 @@ parser.add_argument("URL", help="URL for the GET|POST request")
 parser.add_argument("-o", help="write the body of the response to the specified file.")
 
 args = parser.parse_args()
+print(args)
 
 link = urlparse(args.URL)
 
@@ -69,19 +74,14 @@ link = urlparse(args.URL)
 
 # # decode and display the response
 # print(response.decode("utf-8"))
-
 if args.h:
-    header = dict()
-    header[args.h[0][0].split(":")[0]] = args.h[0][0].split(":")[1] #UGLY and only works for one header. Need to change
+    header = headerToDict(args.h)
 
 if args.get and (args.d or args.f):
     parser.error("GET can't have d or f arguments")
 
-group = subparser_post.add_mutually_exclusive_group(required=True)
-group.add_argument(
-    "-d", help="Associates an inline data to the body HTTP POST request.", metavar="inline-data", action='store')
-group.add_argument(
-    "-f", help="Associates the content of a file to the body HTTP POST request.", metavar="file", action='store')
+elif args.post and not (bool(args.d) != bool(args.f)):
+    parser.error("POST should only have either d or f argument")
 
 elif args.get:
     if args.h:
@@ -99,21 +99,23 @@ elif args.get:
         print(getStuff.text)
    
 elif args.post:
-    print(args.h[0][0].split(":")[0])   #need to change. Really ugly
     if args.d:
         data = args.d
     else:
-        data = args.f
+        data = open(args.f).read()
     if args.h:
         postStuff= requests.post(args.URL, data, headers=header)
     else:
         postStuff = requests.post(args.URL, data)
+    
+    contents = postStuff.headers if "v" in args else '\n' + postStuff.text
+    
     if args.o:
         outputFile = open(args.o, "w")
-        contents = f'{postStuff.headers} \n {postStuff.text}'
         outputFile.write(contents)
         outputFile.close()
     else:
-        if args.verbosity:
-            print(postStuff.headers)
-        print(postStuff.text)
+        print(contents)
+
+# python notcurl.py -post -h Content-Type:application-json -h Nice:One -d '{"number":2}' http://httpbin.org/post
+# python notcurl.py -post -h Content-Type:application-json -h Nice:One -f datafile.json http://httpbin.org/post
