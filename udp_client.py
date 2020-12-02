d9fea2977dd8a30c88d116b87c5d6ac6cb00d946
Siddharth Singh <888.siddharthsingh@gmail.com>
2020-12-01 22:43:08 -0500

    handshake works..?

--- udp_client.py
+++ udp_client.py
@@ -6,39 +6,59 @@ from packet import Packet
 
 
 def run_client(router_addr, router_port, server_addr, server_port):
-    peer_ip = ipaddress.ip_address(socket.gethostbyname(server_addr))
-    conn = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    peer = peer_ip, server_port
-    router = router_addr, router_port
-    timeout = 5
-    try:
-        with open("starwars.txt") as f:
-            file_data = f.read()
-            send(conn, peer, router, file_data)
-            return
+    established = False
+    while not established:
+        peer_ip = ipaddress.ip_address(socket.gethostbyname(server_addr))
+        conn = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        peer = peer_ip, server_port
+        router = router_addr, router_port
+        timeout = 500
+        try:
+            print("trying handshake. Send SYN")
+            p = Packet(packet_type=0,
+                       seq_num=0,
+                       peer_ip_addr=peer_ip,
+                       peer_port=server_port,
+                       payload="Hi S".encode("utf-8"))
+            conn.sendto(p.to_bytes(), (router_addr, router_port))
+            print('Send "{}" to router'.format("Hi S"))
+            # with open("starwars.txt") as f:
+            #     file_data = f.read()
+            #     send(conn, peer, router, file_data)
+            #     return
+            #
+            # msg = "Hello World"
+            # p = Packet(packet_type=0,
+            #            seq_num=1,
+            #            peer_ip_addr=peer_ip,
+            #            peer_port=server_port,
+            #            payload=msg.encode("utf-8"))
+            # conn.sendto(p.to_bytes(), (router_addr, router_port))
+            # print('Send "{}" to router'.format(msg))
+            #
+            # Try to receive a response within timeout
+            conn.settimeout(timeout)
+            print('Waiting for a response')
+            response, sender = conn.recvfrom(1024)
+            p = Packet.from_bytes(response)
+            if p.packet_type == 1:
+                print('Received syn ack, sending ack and data received is {}'.format(p.payload.decode("utf-8")))
+                established = True
+                p_ack = Packet(packet_type=2,
+                               seq_num=0,
+                               peer_ip_addr=peer_ip,
+                               peer_port=server_port,
+                               payload="".encode("utf-8"))
+                conn.sendto(p_ack.to_bytes(), (router_addr, router_port))
+                print("Handshake Done!")
+            # print('Router: ', sender)
+            # print('Packet: ', p)
+            # print('Payload: ' + p.payload.decode("utf-8"))
 
-        msg = "Hello World"
-        p = Packet(packet_type=0,
-                   seq_num=1,
-                   peer_ip_addr=peer_ip,
-                   peer_port=server_port,
-                   payload=msg.encode("utf-8"))
-        conn.sendto(p.to_bytes(), (router_addr, router_port))
-        print('Send "{}" to router'.format(msg))
-
-        # Try to receive a response within timeout
-        conn.settimeout(timeout)
-        print('Waiting for a response')
-        response, sender = conn.recvfrom(1024)
-        p = Packet.from_bytes(response)
-        print('Router: ', sender)
-        print('Packet: ', p)
-        print('Payload: ' + p.payload.decode("utf-8"))
-
-    except socket.timeout:
-        print('No response after {}s'.format(timeout))
-    finally:
-        conn.close()
+        except socket.timeout:
+            print('No response after {}s'.format(timeout))
+        finally:
+            conn.close()
 
 
 # Usage:
@@ -49,7 +69,7 @@ parser.add_argument("--routerhost", help="router host", default="localhost")
 parser.add_argument("--routerport", help="router port", type=int, default=3000)
 
 parser.add_argument("--serverhost", help="server host", default="localhost")
-parser.add_argument("--serverport", help="server port", type=int, default=8007)
+parser.add_argument("--serverport", help="server port", type=int, default=8008)
 args = parser.parse_args()
 
 run_client(args.routerhost, args.routerport, args.serverhost, args.serverport)
--- udp_server.py
+++ udp_server.py
@@ -4,6 +4,13 @@ import socket
 from packet import Packet
 from utils import make_ack, receive
 
+established = False
+TIMEOUT = 5
+SYN = 0
+SYN_ACK = 1
+ACK = 2
+NAK = 3
+FIN = 4
 
 def run_server(port):
     conn = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -19,18 +26,50 @@ def run_server(port):
 
 
 def handle_client(conn, data, sender):
+    global established
     try:
-        request = receive(conn, sender, data)
+        p = Packet.from_bytes(data)
+        peer_ip_addr, peer_port = p.peer_ip_addr, p.peer_port
+        # print("Router: ", sender)
+        # print("Packet: ", p)
+        print("Payload: ", p.payload.decode("utf-8"))
+        if not established:
+            if p.packet_type == SYN:
+                print("Handshake start SYN ")
+                p_synack = Packet(SYN_ACK, 0, peer_ip_addr, peer_port, "Hi R".encode("utf-8"))
+                conn.sendto(p_synack.to_bytes(), sender)
+                print("sending syn_ack")
+            elif p.packet_type == SYN_ACK:
+                print("ACK!!")
+                established = True
+        else:
+            p_ack = Packet(ACK, 0, peer_ip_addr, peer_port, p.payload)
+            conn.sendto(p_ack.to_bytes(), sender)
+            print("Handshake done.")
+        # request = receive(conn, sender, data)
         # How to send a reply.
         # The peer address of the packet p is the address of the client already.
         # We will send the same payload of p. Thus we can re-use either `data` or `p`.
+        # conn.sendto(p.to_bytes(), sender)
 
     except Exception as e:
         print("Error: ", e)
 
 
+def handshake(p, conn, sender):
+    peer_ip_addr, peer_port = p.peer_ip_addr, p.peer_port
+    if p.packet_type == SYN:
+        print("Handshake start SYN ")
+        p_synack = Packet(SYN_ACK, 0, peer_ip_addr, peer_port, p.payload)
+        conn.sendto(p_synack.to_bytes(), sender)
+        print("sending syn_ack")
+    elif p.packet_type == SYN_ACK:
+        print("ACK!!")
+        established = True
+
+
 # Usage python udp_server.py [--port port-number]
 parser = argparse.ArgumentParser()
-parser.add_argument("--port", help="echo server port", type=int, default=8007)
+parser.add_argument("--port", help="echo server port", type=int, default=8008)
 args = parser.parse_args()
 run_server(args.port)
